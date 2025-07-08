50e82e10ae510d494a463d3619bdccf53541321a
Hello <hello@Hellos-MacBook-Air.local>
2025-07-08 17:59:03 +0100

    Update spiderclaws formula to v1.0.2 with correct URL and SHA256

--- Formula/spiderclaws.rb
+++ Formula/spiderclaws.rb
@@ -1,8 +1,8 @@
 class Spiderclaws < Formula
   desc "My custom CLI tool"
   homepage "https://github.com/SpiderClaws44/homebrew-spiderclaws44"
-  url "https://github.com/Brainwayne20/mytool/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "705306abb13f7def7841f950bed533a2a2aeb6aa5f52c535993493ce82ff2b0d  v1.0.1.tar.gz"
+  url "https://github.com/Brainwayne20/mytool/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "b720c1ac7ecc7b0a543d624f7692a5518b8fea5e8ccf3a92599d1347f08b9218  v1.0.2.tar.gz"
   license "MIT"
 
   def install
