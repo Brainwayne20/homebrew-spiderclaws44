a465fe4416592a28fc6cfe90a77ab6212a7f11cd
Hello <hello@Hellos-MacBook-Air.local>
2025-07-08 17:35:37 +0100

    Update spiderclaws formula to v1.0.1

--- Formula/spiderclaws.rb
+++ Formula/spiderclaws.rb
@@ -1,11 +1,16 @@
 class Spiderclaws < Formula
   desc "My custom CLI tool"
   homepage "https://github.com/SpiderClaws44/homebrew-spiderclaws44"
-  url "https://github.com/Brainwayne20/mytool/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "4aeca6fcb752531a65efe29c7edfd62313a31890e43691230d733c6fd54214a6  v1.0.0.tar.gz"
+  url "https://github.com/Brainwayne20/mytool/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "705306abb13f7def7841f950bed533a2a2aeb6aa5f52c535993493ce82ff2b0d  v1.0.1.tar.gz
+"
   license "MIT"
 
   def install
     bin.install "mytool"
+  end
+
+  test do
+    system "#{bin}/mytool"
   end
 end
