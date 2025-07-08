d4e86f4c376a3e5d725c18e352d2967179a338f2
Hello <hello@Hellos-MacBook-Air.local>
2025-07-08 17:20:13 +0100

    Update SHA256 checksum for v1.0.0

--- Formula/spiderclaws.rb
+++ Formula/spiderclaws.rb
@@ -2,7 +2,7 @@ class Spiderclaws < Formula
   desc "My custom CLI tool"
   homepage "https://github.com/SpiderClaws44/homebrew-spiderclaws44"
   url "https://github.com/Brainwayne20/mytool/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed  mytool-v1.0.0.tar.gz"
+  sha256 "4aeca6fcb752531a65efe29c7edfd62313a31890e43691230d733c6fd54214a6  v1.0.0.tar.gz"
   license "MIT"
 
   def install
