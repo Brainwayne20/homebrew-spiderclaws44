1c2ce89aa0a97a2fdc6bc477471013c824dd81a8
Hello <hello@Hellos-MacBook-Air.local>
2025-07-08 16:45:24 +0100

    Add spiderclaws formula

---
+++ Formula/spiderclaws.rb
@@ -0,0 +1,11 @@
+class Spiderclaws < Formula
+  desc "My custom CLI tool"
+  homepage "https://github.com/SpiderClaws44/homebrew-spiderclaws44"
+  url "https://github.com/SpiderClaws44/mytool/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "REPLACE_WITH_REAL_SHA256"
+  license "MIT"
+
+  def install
+    bin.install "mytool"
+  end
+end
